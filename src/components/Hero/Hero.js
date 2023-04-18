291b9e7af6d1b69e232390e4319daa98a0da5b13
Dominika Misiuda <misiuda.d@gmail.com>
2023-04-18 21:28:25 +0200

    init project&add components

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import styles from './Hero.module.scss';
+
+const Hero = () => {
+    return (
+      <div>
+        <h1>My first React App</h1>
+        <p>A simple to-do app, with lists, columns and card</p>
+      </div>
+    );
+  };
+
+  export default Hero;
