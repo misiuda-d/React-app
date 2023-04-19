4c96b2fcadec2af97451781cba8cceeee254a5ac
Dominika Misiuda <misiuda.d@gmail.com>
2023-04-19 21:40:28 +0200

    add subpages & 404 not found

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Hero from './components/Hero/Hero.js';
-import SearchForm from './components/SearchForm/SearchForm.js';
-import List from './components/List/List.js';
-import Container  from './components/Container/Container';
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Favorite from './components/Favorite/Favorite';
+import About from './components/About/About';
+import Container from './components/Container/Container';
+import NavBar from './components/NavBar/NavBar';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 
 const App = () => {
   return (
-    <Container>
-      <Hero />
-      <SearchForm />
-      <List />
+<main>
+      <NavBar />
+      <Container>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </Container>
+    </main>
   );
 };
 
